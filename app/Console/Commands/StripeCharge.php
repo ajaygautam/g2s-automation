e9d0c16f0b9bba67438bb8e709809badc3ff3495
Sarabjeet Dhillon <sarabjeet.dhillon@sdnatech.com>
2019-10-21 19:43:07 +0530

    cron job fix

--- app/Console/Commands/StripeCharge.php
+++ app/Console/Commands/StripeCharge.php
@@ -45,87 +45,38 @@ class StripeCharge extends Command
      */
     public function handle()
     {
+        // DB::connection()->enableQueryLog();
+        $customers = User::with('membership','customerPlan','food_drink_charges_monthly_total','peak_hours_usage','off_peak_hours_usage')
+        ->where('customer_type','4')  
+        ->orderBy('id','desc')
+        ->get();
 
-        // die('here');
-
-        $stripe_secret = config('settings');
-
-        Stripe::setApiKey($stripe_secret);
-         
-        DB::connection()->enableQueryLog();
-        $customers = User::with('membership','peak_hours_usage','off_peak_hours_usage')
-                        ->get();
-        
-                        $queries = DB::getQueryLog();
-        allQuery($queries);
-        
-        die;                
-        
-            
-        $resource = Resource::find('1');    
-        $peak_hour_charge = $resource->peak_price; 
-        $off_peak_hour_charge = $resource->off_peak_price;
-
+        // TODO: fetch customers whose day(plan_starts_on) is today 
 
 
         foreach($customers as $customer){
-           //process only members
-          
-            if(count($customer->membership)>0)
-            {
-                if($customer->membership[0]->frequency == 0){
-                   //fetch discount 
-                    $discount_play = $customer->membership[0]->play_discount;  //in percent
-                
-                    $used_peak_hours = $used_off_peak_hours = 0;
-                
-                    if(count($customer->peak_hours_usage)){
-                        $used_peak_hours = ($customer->peak_hours_usage[0]->peak_hours_used); // covert minutes to hours
-                    }
-                    if(count($customer->off_peak_hours_usage)){
-                        $used_off_peak_hours = ($customer->off_peak_hours_usage[0]->off_peak_hours_used); //convert minutes to hours
-                    }
-                
-                    $included_peak_hours = $customer->membership[0]->included_peak_hours;
-                    $included_off_peak_hours = $customer->membership[0]->included_off_peak_hours;
-    
-                    $additional = (($used_peak_hours - $included_peak_hours) * $peak_hour_charge) +  (($used_off_peak_hours - $included_off_peak_hours) * $off_peak_hour_charge);
-                    $additional_final = $additional - ($additional * $discount_play)/100;
-        
-                    if($additional_final>0){
-                        $chargable_amount = $customer->membership[0]->cost + $additional_final;
-                    }
-                    else{
-                        $chargable_amount = $customer->membership[0]->cost;
-                    }
+        //process only members
 
-                    Log::info($customer->id.' -> '.$chargable_amount);
+                $chargable_amount = self::CalculateChargeableOverageAmount($customer);
+                // echo "<br />";
 
-                    if($chargable_amount > 0){
-                        $charge = Charge::create([
-                            "amount" => $chargable_amount * 100, //convert into cents
-                            "currency" => "usd",
-                            "customer" => $customer->stripe_customer_id,
-                            "description" => "Monthly membership charges"
+                //    die;
+                if($chargable_amount > 0){
+                    $request = new stdClass();
+                    $request->stripe_charge_comment = "Monthly charge";
+                    try{
+                        $payment = self::StripeCharge([
+                        'customer' => $customer,
+                        'amount' => $chargable_amount,
+                        'comments' => "Membership cost",
                         ]);
-        
-                        if($charge){
-                            $payment = Payment::create([
-                                'customer_id' => $customer->id,
-                                'amount' => $chargable_amount,
-                                'payment_status' => $charge->status,
-                                'stripe_charge_id' => $charge->id,
-                                'stripe_receipt_url' => $charge->receipt_url,
-                                'stripe_currency' => $charge->currency,
-                                'stripe_invoice_number' => $charge->invoice
-                            ]);
-                        }    
-                    }      
-                } //frequency==0
 
-               
-            }
-           
+                    }catch(Exception $e){
+                    // echo "dasdas";
+                    // return throwExpection($e);
+                    }
+                }
+            } 
         }  
     }
 }
